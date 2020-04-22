bde466def70af3f0594bb7cec60cf39b801ea217
Efremov-omk <respectewe@gmail.com>
2020-04-22 13:29:44 +0300

    Laravel урок №6: [ Seeds - заполнение БД тестовыми данными ]

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(BlogCategoriesTableSeeder::class);
+        factory(BlogPost::class, 100)->create();
     }
 }
